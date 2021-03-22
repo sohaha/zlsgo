66c7845a3b3717c0e8856c71dae7a65d3192782a
影浅 <soyz@qq.com>
2021-03-22 15:01:41 +0800

    feat: ✨ check color under windows

---
+++ zlog/color_win.go
@@ -0,0 +1,77 @@
+// +build windows
+
+package zlog
+
+import (
+	"syscall"
+)
+
+var (
+	winEnable          = isSupportColor()
+	procGetConsoleMode *syscall.LazyProc
+	procSetConsoleMode *syscall.LazyProc
+)
+
+func init() {
+	if winEnable {
+		return
+	}
+
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
+	procSetConsoleMode = kernel32.NewProc("SetConsoleMode")
+
+	winEnable = tryApplyOnCONOUT()
+	if !winEnable {
+		winEnable = tryApplyStdout()
+	}
+}
+
+func tryApplyOnCONOUT() bool {
+	outHandle, err := syscall.Open("CONOUT$", syscall.O_RDWR, 0)
+	if err != nil {
+		return false
+	}
+
+	err = EnableTerminalProcessing(outHandle, true)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func tryApplyStdout() bool {
+	err := EnableTerminalProcessing(syscall.Stdout, true)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func EnableTerminalProcessing(stream syscall.Handle, enable bool) error {
+	var mode uint32
+	err := syscall.GetConsoleMode(stream, &mode)
+	if err != nil {
+		return err
+	}
+
+	if enable {
+		mode |= 0x4
+	} else {
+		mode &^= 0x4
+	}
+
+	ret, _, err := procSetConsoleMode.Call(uintptr(stream), uintptr(mode))
+	if ret == 0 {
+		return err
+	}
+
+	return nil
+}
+
+// IsSupportColor IsSupportColor
+func IsSupportColor() bool {
+	return !DisableColor && winEnable
+}
