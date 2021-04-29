c4ba6452e0a98f4eb85ea1d3200f7b7f15a5e087
seekwe <soseek@vip.qq.com>
2021-04-29 17:31:36 +0800

    fix: 🐛 runtime test

--- zutil/runtime_test.go
+++ zutil/runtime_test.go
@@ -1,16 +1,18 @@
 package zutil_test
 
 import (
-	"github.com/sohaha/zlsgo/zutil"
 	"testing"
+
+	"github.com/sohaha/zlsgo/zutil"
 )
 
 func TestGetGid(t *testing.T) {
 	t.Log(zutil.GetGid())
-	var c chan struct{}
+	c := make(chan struct{}, 0)
 	go func() {
 		t.Log(zutil.GetGid())
 		c <- struct{}{}
 	}()
 	<-c
+	t.Log(zutil.GetGid())
 }
