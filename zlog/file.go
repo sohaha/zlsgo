a11840f08280dcee79c0fd4b8ae547f823e89887
seekwe <seekwe@gmail.com>
2022-11-24 20:05:16 +0800

    ✨ feat: The log file for more than the biggest save time automatically removed

--- zlog/file.go
+++ zlog/file.go
@@ -6,11 +6,12 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/sohaha/zlsgo/zfile"
 	"github.com/sohaha/zlsgo/ztime"
 )
+
+var LogMaxDurationDate = 15
 
 func openFile(filepa string, archive bool) (file *zfile.MemoryFile, fileName, fileDir string, err error) {
 	fullPath := zfile.RealPath(filepa)
@@ -21,8 +22,38 @@ func openFile(filepa string, archive bool) (file *zfile.MemoryFile, fileName, fi
 		base := strings.TrimSuffix(fileName, ext)
 		fileDir = zfile.RealPathMkdir(fileDir+base, true)
 		fullPath = fileDir + fileName
+		lastArchiveName := ""
 		opt = append(opt, zfile.MemoryFileFlushBefore(func(f *zfile.MemoryFile) error {
-			archiveName := ztime.FormatTime(time.Now(), "Y-m-d")
+			archiveName := ztime.Now("Y-m-d")
+			if lastArchiveName != archiveName {
+				if lastArchiveName != "" {
+					// Delete the log file that is too old
+					go func() {
+						now := ztime.UnixMicro(ztime.Clock())
+						_ = filepath.Walk(fileDir, func(path string, info os.FileInfo, err error) error {
+							if err != nil {
+								return err
+							}
+							if info.IsDir() {
+								return nil
+							}
+
+							// The log may have been modified, so the modification time of the file is no longer used here.
+							// if info.ModTime().AddDate(0, 0, LogMaxDurationDate).Before(now) {
+							// 	_ = os.Remove(path)
+							// }
+
+							date, err := ztime.Parse(strings.TrimSuffix(filepath.Base(path), ext), "Y-m-d")
+							if err == nil && date.AddDate(0, 0, LogMaxDurationDate).Before(now) {
+								_ = os.Remove(path)
+							}
+							return nil
+
+						})
+					}()
+				}
+				lastArchiveName = archiveName
+			}
 			fileName = archiveName + ext
 			f.SetName(fileDir + fileName)
 			return nil
@@ -42,7 +73,6 @@ func (log *Logger) SetFile(filepath string, archive ...bool) {
 func (log *Logger) setLogfile(filepath string, archive bool) {
 	fileObj, fileName, fileDir, _ := openFile(filepath, archive)
 	log.mu.Lock()
-	defer log.mu.Unlock()
 	log.CloseFile()
 	log.file = fileObj
 	log.fileDir = fileDir
@@ -52,6 +82,7 @@ func (log *Logger) setLogfile(filepath string, archive bool) {
 	} else {
 		log.out = fileObj
 	}
+	log.mu.Unlock()
 }
 
 func (log *Logger) Discard() {
