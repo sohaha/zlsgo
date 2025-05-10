e5cda95784db2ab615102d1adef9555e2a61d7e3
seekwe <seekwe@gmail.com>
2025-05-10 18:13:29 +0800

    🔧 refactor: When failing to obtain Cloudflare IP, use the built-in data directly.

--- znet/realip/cloudflare.go
+++ znet/realip/cloudflare.go
@@ -10,19 +10,25 @@ import (
 	"github.com/sohaha/zlsgo/zsync"
 )
 
-func GetCloudflare() []string {
+func GetCloudflare(timeout ...time.Duration) []string {
 	const (
 		cfIPv4CIDRsEndpoint = "https://www.cloudflare.com/ips-v4"
 		cfIPv6CIDRsEndpoint = "https://www.cloudflare.com/ips-v6"
 	)
 
 	var (
+		t       time.Duration
 		wg      zsync.WaitGroup
 		mu      = zsync.NewRBMutex()
 		cfCIDRs = make([]string, 0)
 	)
+	if len(timeout) > 0 {
+		t = timeout[0]
+	} else {
+		t = time.Second * 5
+	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	ctx, cancel := context.WithTimeout(context.Background(), t)
 	defer cancel()
 
 	for _, v := range []string{cfIPv4CIDRsEndpoint, cfIPv6CIDRsEndpoint} {
@@ -38,6 +44,10 @@ func GetCloudflare() []string {
 	}
 
 	wg.Wait()
+
+	if len(cfCIDRs) == 0 {
+		cfCIDRs = []string{"173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "141.101.64.0/18", "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20", "197.234.240.0/22", "198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/13", "104.24.0.0/14", "172.64.0.0/13", "131.0.72.0/22", "2400:cb00::/32", "2606:4700::/32", "2803:f800::/32", "2405:b500::/32", "2405:8100::/32", "2a06:98c0::/29", "2c0f:f248::/32"}
+	}
 
 	return cfCIDRs
 }
--- znet/realip/cloudflare_test.go
+++ znet/realip/cloudflare_test.go
@@ -2,8 +2,10 @@ package realip
 
 import (
 	"testing"
+	"time"
 )
 
 func TestGetCloudflare(t *testing.T) {
 	t.Log(GetCloudflare())
+	t.Log(GetCloudflare(time.Microsecond))
 }
